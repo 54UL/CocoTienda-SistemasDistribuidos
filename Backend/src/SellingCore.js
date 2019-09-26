eb9ba4c0288fde4a91a7ea70d88f10c7a122df58
Diego Pacheco <A15100268@live.ceti.mx>
2019-09-26 12:56:24 -0500

    BACKEND FRONT CONNECTED!!!!!

--- Backend/src/SellingCore.js
+++ Backend/src/SellingCore.js
@@ -4,19 +4,31 @@ var express = require('express')
 var  usrApi = require('./Usuarios.js')
 var  mwApi  = require('./BDMiddleWareApi.js')
 
-
-
 //GLOBAL INITIALIZATION CODE
 var  app = express();
 var  bdApi = mwApi.globalApiManager.getApi("highlevel");
 
-app.get("/Login/Authenticate/:usuario/:pass",function(req,res)
+
+
+
+// CORS HEADER SETUP
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'null');
+    // Pass to next layer of middleware
+    next();
+});
+
+
+
+
+app.get("/Login/Authenticate/:usuario/:pass",function(req, res)
 {
    var usuario = req.params.usuario;
    var pass = req.params.pass;
-   console.log("usr :"+usuario+" pass"+pass);
-   usrApi.logIn(usuario,pass);
-   res.send({usuario,pass});
+   console.log("usr:" + usuario + " pass" + pass);
+   usrApi.logIn(usuario, pass);
+   res.send({usuario, pass});
   
 });
 
@@ -26,16 +38,23 @@ app.get('/', function (req, res) {
     res.send('hello world');
 });
   
-  
-
 
 
 app.get('/ProductSelling/retrive/:category', function (req, res) 
 {
    var category = req.params.category;
-  
    var arregloJSONPrueba =
-   {"productos":[{nombre:"taza2",precio:"$666.6"},{nombre:"taza2",precio:"$666.6"},{nombre:"taza3",precio:"$666.6"},{nombre:"taza4",precio:"$666.6"}]};
+   { "productos":[
+                {nombre:"taza2",precio:"$666.6"},
+                {nombre:"taza2",precio:"$234432.6"},
+                {nombre:"taza3",precio:"$985.6"},
+                {nombre:"taza100",precio:"$403.8"},
+                {nombre:"taza100",precio:"$403.8"},
+                {nombre:"taza100",precio:"$403.8"},
+                {nombre:"taza100",precio:"$403.8"},
+                {nombre:"taza100",precio:"$403.8"}
+                ]
+    };
 
    res.json(arregloJSONPrueba);
 });
@@ -44,7 +63,6 @@ app.get('/ProductSelling/retrive/:category', function (req, res)
 //RUN THE SERVER
 app.listen(3000,function()
 {
-
    console.log("server ready in port 3000");
 }
 );
--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,40 +1,46 @@
-function productoComponent(ModeloProucto)
+function productoComponent(ModeloProducto)
 				{
 					return  "<div class='col-md-3 col-xs-6'>" +
-							"<div class='product'>"+
-							"<div class='product-img'>"+
-							"<img src='img/Llaveros/Lguitarra.jpg' alt=''>"+
+							    "<div class='product'>"+
+							        "<div class='product-img'>"+
+							            "<img src='img/Llaveros/Lguitarra.jpg' alt=''>"+
+							        "</div>"+
+							    "<div class='product-body'>"+
+							        "<p class='product-category'>SHIT</p>"+
+							            "<h3 class='product-name'><a href='#'>"+ModeloProducto.nombre+"</a></h3>"+
+							            "<h4 class='product-price'>"+ModeloProducto.precio+"</h4>"+
+							                "<div class='product-btns'>"+
+                                                "<button class='add-to-wishlist'><i class='fa fa-heart-o'></i></button>"+
+                                                "<button class='add-to-cart'><i class='fa fa-shopping-cart'></i></button>"+
+							                "</div>"+
+							    "</div>"+
+							    "</div>"+
 							"</div>"+
-							"<div class='product-body'>"+
-							"<p class='product-category'>SHIT</p>"+
-							"<h3 class='product-name'><a href='#'>"+ModeloProucto.nombre+"</a></h3>"+
-							"<h4 class='product-price'>"+ModeloProucto.precio+"</h4>"+
-							"<div class='product-btns'>"+
-							"<button class='add-to-wishlist'><i class='fa fa-heart-o'></i></button>"+
-							"<button class='add-to-cart'><i class='fa fa-shopping-cart'></i></button>"+
-							"</div>"+
-							"</div>"+
-							"</div>"+
-							"</div>"+
-							"<div class='clearfix visible-sm visible-xs'></div>"
+							"<div class='clearfix visible-sm visible-xs'>" + 
+                            "</div>"
 				}
-			
+			    
+                
 				function loadProducts()
 				{
-			     
-			     // peticion http al back
-
-				 //Dummy test del modelo de productos
-				 //var arregloProductos = http("post","localhost/productSelling/retriveAll"); <-pseudo code
-				var arregloJSONPrueba =
-				{"productos":[{nombre:"taza1",precio:"$666.6"},{nombre:"taza2",precio:"$666.6"},{nombre:"taza3",precio:"$666.6"},{nombre:"taza4",precio:"$666.6"}]};
-
-				//var arregloModelosProductos = JSON.parse(arregloProductosDummy); esto solamente se usa cuando el json venga de un request 
-					for(var i =0; i<arregloJSONPrueba.productos.length;i++)
-					{
-						$("#containerProductos").append(productoComponent(arregloJSONPrueba.productos[i]));
-					
-					}
+                        var xhr = new XMLHttpRequest();
+                        
+                    xhr.open("GET","http://localhost:3000/ProductSelling/retrive/0)                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ");
+                    xhr.send();
+                    xhr.onreadystatechange= function()
+                    {
+                        if(this.readyState ==4 && this.status ==200)
+                        {
+                            var jsonProductos =   JSON.parse(this.responseText);
+                            console.log(jsonProductos);
+                            //console.log("numero aleatorio" +this.getResponseHeader("holaxd"));\
+                            for(var i =0; i<jsonProductos.productos.length;i++)
+                            {
+                                $("#containerProductos").append(productoComponent(jsonProductos.productos[i]));
+                            
+                            }
+                        }
+                    }
 				}
 
 				window.onload = function() {
