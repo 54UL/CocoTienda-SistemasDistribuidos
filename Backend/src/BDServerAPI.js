c3d720592f80275bef1567d24b27a709da5cdd6f
Diego Pacheco <A15100268@live.ceti.mx>
2019-10-20 15:08:24 -0500

    Se quito el sidebar

--- Backend/src/BDServerAPI.js
+++ Backend/src/BDServerAPI.js
@@ -3,79 +3,70 @@
 //to do's 
 // definir interfas de http express, con estas implementaciones (van en bdServer.JS)
 //VARIABLES
-var mysql      = require('mysql2/promise');
-var  mwApi     = require('./BDMiddleWareApi.js')
-//se define esta variable para no tener que hacer get api todo el rato :v
-var api; 
+var mysql = require('mysql2/promise');
+var mwApi = require('./BDMiddleWareApi.js')
+    //se define esta variable para no tener que hacer get api todo el rato :v
+var api;
 
 //import colorsCodes  from './colorCodes'
 var colorCodes = require("./colorCodes");
 var colors = colorCodes.colors;
 
-function init()
-{
-   try {
-      //AÑADIMOS LA API QUE FUNCIONA DE LADO DE LA APLICACION DE NODE
-      mwApi.globalApiManager.addApi(new mwApi.BDMiddleWareAPI(
-          this.apiName = "lowlevel",
-          this.bdenpoint = "",
-          this.user = "root",
-          this.pass = "",
-          this.query = bdQueryl,
-          this.config = bdConfigureParametersl,
-          this.connect = bdConnectl,
-          this.close = closel
-      )); 
-     api = mwApi.globalApiManager.getApi("lowlevel");
-      
-   } catch (error) {
-      console.error(new Error(colors.yellow+ "BDServerApi -> "+colors.red + error));
-   }
+function init() {
+    try {
+        //AÑADIMOS LA API QUE FUNCIONA DE LADO DE LA APLICACION DE NODE
+        mwApi.globalApiManager.addApi(new mwApi.BDMiddleWareAPI(
+            this.apiName = "lowlevel",
+            this.bdenpoint = "",
+            this.user = "",
+            this.pass = "",
+            this.query = bdQueryl,
+            this.config = bdConfigureParametersl,
+            this.connect = bdConnectl,
+            this.close = closel
+        ));
+        api = mwApi.globalApiManager.getApi("lowlevel");
+
+    } catch (error) {
+        console.error(new Error(colors.yellow + "BDServerApi -> " + colors.red + error));
+    }
 }
 
 //SQL IMPLEMENTATION
 var sql_connection;
 
-async function bdQueryl(query)
-{ 
-   try {
-      let [rows, fields] = await sql_connection.execute(query);
-      return rows;
-      
-   } catch (error) {
-      console.error(new Error(colors.yellow+ "BDServerApi -> "+colors.red + error));
-   }
+async function bdQueryl(query) {
+    try {
+        let [rows, fields] = await sql_connection.execute(query);
+        return rows;
+
+    } catch (error) {
+        console.error(new Error(colors.yellow + "BDServerApi -> " + colors.red + error));
+    }
 }
 
-function bdConnectl()
-{
-   // sql_connection.connect();
+function bdConnectl() {
+    // sql_connection.connect();
 }
 
-async function bdConfigureParametersl()
-{
-   try {
-      sql_connection = await mysql.createConnection({
-         host     :"localhost",
-         user     :"root",
-         password :"",
-         database :"giftstoredb"
-       });
-     
-       return sql_connection ? false:true;
-      
-   } catch (error) {
-      console.error(new Error(colors.yellow+ "BDServerApi -> "+colors.red + error));
-      console.log(colors.blue + "Is mysql closed?");
-   }
+async function bdConfigureParametersl() {
+    try {
+        sql_connection = await mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "",
+            database: "giftstoredb"
+        });
+
+        return sql_connection ? false : true;
+
+    } catch (error) {
+        console.error(new Error(colors.yellow + "BDServerApi -> " + colors.red + error));
+        console.log(colors.blue + "Is mysql closed?");
+    }
 }
 
-function closel()
-{
-   sql_connection.close();
+function closel() {
+    sql_connection.close();
 }
 module.exports.init = init;
-
-
-
-
