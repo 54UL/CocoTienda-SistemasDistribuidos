7a1d30ea02bbbe66fe51d17e214f7d06259f71d1
Gis <carpinteiro.gisel@gmail.com>
2019-10-30 11:50:51 -0600

    crudLibros.js

---
+++ FrontEnd/js/crudLibros.js
@@ -0,0 +1,76 @@
+function usuarioComponent(ModeloUsuario) {
+    return "<div class='col-md-12 col-xs-12'>" +
+        "<table class='table'>" +
+        "<thead>" +
+        " <tr>" +
+        " <th scope='col'>Id</th>" +
+        " <th scope='col'>Nombre</th>" +
+        "<th scope='col'>Correo</th>" +
+        "<th scope='col'>Tipo de Usuario</th>" +
+
+        "<th scope='col'></th>" +
+        "</tr>" +
+        "</thead>" +
+        "<tbody>" +
+        "<tr>" +
+        "<th scope='row'>" + ModeloUsuario.id_usuario + "</th>" +
+        " <td>" + ModeloUsuario.nombre + "</td>" +
+        " <td>" + ModeloUsuario.correo + "</td>" +
+        " <td>" + ModeloUsuario.id_tipousuario + "</td>" +
+
+        "<td>" +
+        "<form><button type='button'>Editar</form>" +
+        " </td>" +
+        " <td>" +
+        "<form><button type='button'>Eliminar</form>" +
+        "</td>" +
+        "</tr>" +
+        "</tbody>" +
+        "</table>" +
+        "</div>"
+}
+
+function loadUsuarios() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", endpoint("/Users/GetAllUsers"));
+    xhr.send();
+    $("#containerProductos").html("<h1>CARGANDO...</h1>");
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            $("#containerUsuarios").html("");
+            var jsonUsuarios = JSON.parse(this.responseText);
+            console.log(jsonUsuarios);
+            for (var i = 0; i < jsonUsuarios.usuarios.length; i++) {
+                var actualModel = jsonUsuarios.usuarios[i];
+                $("#containerUsuarios").append(usuarioComponent(actualModel));
+            }
+        }
+    }
+}
+
+
+function deleteUsuario(id_usuario, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", endpoint("/Users/Delete/" + id_usuario));
+
+    // xhr.open("GET", endpoint("/ProductSelling/buy/" + productoID + "/" + token));
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var borrar = JSON.parse(this.responseText);
+            callback(borrar)
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+window.onload = function() {
+    loadUsuarios();
+}
