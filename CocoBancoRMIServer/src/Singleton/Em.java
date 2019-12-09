b7c75f1db58ce849c4e556b0bdcd630e290a885c
Isaac <a16100339@live.ceti.mx>
2019-12-09 03:55:13 -0600

    CocoBanco RMI

---
+++ CocoBancoRMIServer/src/Singleton/Em.java
@@ -0,0 +1,16 @@
+package Singleton;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+
+public class Em {
+    private static EntityManager em;      
+    private Em(){}
+    
+    static public EntityManager get(){
+        if(em == null)         
+            em = Persistence.createEntityManagerFactory("CocoBancoRMIServerPU").createEntityManager();                
+        return em;               
+    }
+}
