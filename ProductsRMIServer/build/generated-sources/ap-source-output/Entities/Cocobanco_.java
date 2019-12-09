a3fdcd1a201c538d5f42af5a914c3ef5ecf7c58c
Fernando Gómez <fernandozazir@gmail.com>
2019-12-09 03:12:12 -0600

    gg

---
+++ ProductsRMIServer/build/generated-sources/ap-source-output/Entities/Cocobanco_.java
@@ -0,0 +1,16 @@
+package Entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-09T01:40:00")
+@StaticMetamodel(Cocobanco.class)
+public class Cocobanco_ { 
+
+    public static volatile SingularAttribute<Cocobanco, String> correo;
+    public static volatile SingularAttribute<Cocobanco, String> contrasenia;
+    public static volatile SingularAttribute<Cocobanco, Integer> iDCuenta;
+    public static volatile SingularAttribute<Cocobanco, Double> saldo;
+
+}
---
+++ ProductsRMIServer/build/generated-sources/ap-source-output/Entities/Cuentas_.java
@@ -0,0 +1,16 @@
+package Entities;
+
+import Entities.Usuario;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-09T01:40:00")
+@StaticMetamodel(Cuentas.class)
+public class Cuentas_ { 
+
+    public static volatile SingularAttribute<Cuentas, Usuario> iDUsuarioGift;
+    public static volatile SingularAttribute<Cuentas, Integer> iDCuenta;
+    public static volatile SingularAttribute<Cuentas, Integer> iDCuentas;
+
+}
